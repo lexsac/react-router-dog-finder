3788cc37531820cd4ff0d8c1228f7ee2b6b0e4ea
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-14 11:09:18 -0700

    Added routes, nav, and defaultProps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import './App.css';
+import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
+import FilterDog from './FilterDog';
+import NavBar from './NavBar';
+import DogsList from './DogsList';
+import whiskey from "./images/whiskey.jpg";
+import tubby from "./images/tubby.jpg";
+import duke from "./images/duke.jpg";
+import perry from "./images/perry.jpg";
 
-function App() {
+
+function App({ dogs }) {
   return (
     <div className="App">
-      <h1>Hi!</h1>
+      <BrowserRouter>
+        <NavBar dogs={dogs} />
+        <Switch>
+          <Route exact path="/dogs"><DogsList dogs={dogs} /></Route>
+          <Route exact path="/dogs:name"><FilterDog dogs={dogs} /></Route>
+          {/* <Redirect to="/dogs" /> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
+
+export const dogs = [
+  {
+    name: "Whiskey",
+    age: 5,
+    src: whiskey,
+    facts: [
+      "Whiskey loves eating popcorn.",
+      "Whiskey is a terrible guard dog.",
+      "Whiskey wants to cuddle with you!"
+    ]
+  },
+  {
+    name: "Duke",
+    age: 3,
+    src: duke,
+    facts: [
+      "Duke believes that ball is life.",
+      "Duke likes snow.",
+      "Duke enjoys pawing other dogs."
+    ]
+  },
+  {
+    name: "Perry",
+    age: 4,
+    src: perry,
+    facts: [
+      "Perry loves all humans.",
+      "Perry demolishes all snacks.",
+      "Perry hates the rain."
+    ]
+  },
+  {
+    name: "Tubby",
+    age: 4,
+    src: tubby,
+    facts: [
+      "Tubby is really stupid.",
+      "Tubby does not like walks.",
+      "Angelina used to hate Tubby, but claims not to anymore."
+    ]
+  }
+];
+
+App.defaultProps = { dogs };
 
 export default App;
